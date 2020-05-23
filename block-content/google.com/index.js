d0a144f6b822796367e63943dc005c67c6810a4d
Widi Harsojo <wharsojo@gmail.com>
2020-05-24 01:00:15 +0800

    add google route

---
+++ block-content/google.com/index.js
@@ -0,0 +1,23 @@
+const googlJS = function() {
+  document.querySelectorAll('g-section-with-header').forEach(n=>n.remove())
+  document.querySelectorAll('.obcontainer').forEach(n=>n.remove())
+  document.querySelectorAll('.g-blk').forEach(n=>n.remove())
+};
+
+const domain =  __dirname.split(/\\|\//).pop();
+const {hello} = global.mitm.fn;
+
+routes = {
+  title: 'Search - google',
+  url: 'https://google.com/search?q=github+playwright',
+  html: {
+    'www.google.com/search': {
+      el: 'e_end', //or e_head
+      js: [googlJS, hello], //JS at end of html body
+    },
+  },
+  js: {
+    'gstatic.com': ''
+  }
+}
+global.mitm.fn.routeSet(routes, domain, true)
